0bac8a0883790545a5884817d8e70c8c73fa9b9f
lorencalebbaum <lorencalebbaum@cedarville.edu>
2016-05-06 13:30:10 -0400

    I added class

--- ConsoleApplication2/GPA.cpp
+++ ConsoleApplication2/GPA.cpp
@@ -13,9 +13,15 @@ int main() {
 	double totalCredits;
 	double semesterCredtis;
 	vector<double> credits;
+	int option = 0;
 
 	cout << "Do you want to calculate: \n" << "(1) The current semesters GPA?\n"
 		<< "(2) Total GPA?\n";
+	cin >> option;
+
+	if (option == 1) {
+		
+	}
 
 	return 0;
 }
---
+++ ConsoleApplication2/classGrades.h
@@ -0,0 +1,27 @@
+//Created by: Caleb Baum
+//Purpose: To create a new type for Grades in Classes
+//Date: 5/4/2016 - Created Program
+
+#ifndef GRADES
+#define GRADES
+
+#include <string>
+#include <iostream>
+#include <math.h>
+
+struct LetterGrade {
+
+};
+
+using namespace std;
+
+class Grades {
+public:
+	Grades();
+
+private:
+	double creditHrs;
+	
+};
+
+#endif // GRADES
