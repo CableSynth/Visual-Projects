cb10150bf432939885920feeabacf18e62b2ffda
lorencalebbaum <lorencalebbaum@cedarville.edu>
2016-05-06 13:04:31 -0400

    Add project files.

---
+++ ConsoleApplication2/GPA.cpp
@@ -0,0 +1,20 @@
+//Created by: Caleb Baum
+//Purpose: To Calculate GPA
+//Date: 5/4/2016 - Created Program
+
+#include <iostream>
+#include<string>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+	double totalCredits;
+	double semesterCredtis;
+	vector<double> credits;
+
+
+
+	return 0;
+}
